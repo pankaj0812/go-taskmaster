5e21fbc9b3acd4167337ce6fa66799008e0a16b7
pankaj0812 <singhpankajkumar65@gmail.com>
2024-06-14 19:09:51 +0530

    Tested skeleton code

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"github.com/pankaj0812/go-taskmaster/manager"
+	"github.com/pankaj0812/go-taskmaster/node"
+	"github.com/pankaj0812/go-taskmaster/task"
+	"github.com/pankaj0812/go-taskmaster/worker"
+)
+
+func main() {
+	t := task.Task{
+		ID:     uuid.New(),
+		Name:   "Task-1",
+		State:  task.Pending,
+		Image:  "Image-1",
+		Memory: 1024,
+		Disk:   1,
+	}
+
+	te := task.TaskEvent{
+		ID:        uuid.New(),
+		State:     task.Pending,
+		Timestamp: time.Now(),
+		Task:      t,
+	}
+
+	fmt.Printf("Task: %v\n", t)
+	fmt.Printf("TaskEvent: %v\n", te)
+
+	w := worker.Worker{
+		Name:  "worker-1",
+		Queue: *queue.New(),
+		Db:    make(map[uuid.UUID]*task.Task),
+	}
+	fmt.Printf("Worker: %v\n", w)
+	w.CollectStats()
+	w.RunTask()
+	w.StartTask()
+	w.StopTask()
+
+	m := manager.Manager{
+		Pending: *queue.New(),
+		TaskDb:  make(map[string][]*task.Task),
+		EventDb: make(map[string][]*task.TaskEvent),
+		Workers: []string{w.Name},
+	}
+
+	fmt.Printf("Manager: %v\n", m)
+	m.SelectWorker()
+	m.UpdateTasks()
+	m.SendWork()
+
+	n := node.Node{
+		Name:   "Node-1",
+		Ip:     "192.168.1.1",
+		Cores:  4,
+		Memory: 1024,
+		Disk:   25,
+		Role:   "worker",
+	}
+
+	fmt.Printf("node: %v\n", n)
+}
